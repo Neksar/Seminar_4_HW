4926ceee1360774c75379e47041d47cd43cbc4a6
Maksim <m.bondarenko1988@yandex.ru>
2023-01-29 10:31:33 +0300

    Done second homework to fourth seminar about Csharp

---
+++ homework_2/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int InputNumber(string str)
+{
+    int number;
+    //string?  text;
+    while(true)
+    {
+        System.Console.Write(str);
+        if(int.TryParse(Console.ReadLine(), out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Вы ввели не число");
+    }
+    return number;
+}
+
+int SumChar(int num)
+{
+    int result = 0;
+    while (num > 0)
+    {
+        result += num % 10;
+        num = num / 10;
+    }
+    return result;
+
+}
+
+int num = InputNumber("Введите Ваше число!: ");
+int result = SumChar(num);
+
+System.Console.WriteLine($"Сумма цифр числа {num} равно: {result}");
+
